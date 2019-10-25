f652d7662cdc29e69b6eaf6ab9ea0565675fa113
Brandon Olivier <brandon@brandonolivier.com>
2019-10-25 18:56:07 -0500

    Add my blog post and set up some defaults

---
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+import { Styled } from 'theme-ui';
+
+/**
+ * Shadow me to add your own bio content
+ */
+
+export default () => (
+  <Fragment>
+    I write Javascript for work, and Clojure(script) for fun.
+    <br />
+    Check out my resume
+  </Fragment>
+);
